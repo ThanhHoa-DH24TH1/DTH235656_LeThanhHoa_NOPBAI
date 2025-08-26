d2635257e356aa52d66a51ddecbc5bbe0cf47583
DTH235656_Lê Thanh Hòa <hoa_dth235656@student.agu.edu.vn>
2025-08-26 20:21:44 +0700

    Add files via upload

---
+++ BaitapChuong1.py
@@ -0,0 +1,32 @@
+import sys
+import math
+import os
+
+print("Hello World!\n")  
+
+print("Albert Einstein đã từng nói: Một người không bao giờ mắc sai lầm không bao giờ thử bất kì điều gì mới.\n")
+
+if 5>2 :
+    print("Five is greater than tow!\n")
+
+total= 1 + \
+2 + \
+3
+print(total);print("Hello Python!\n")
+
+if True:
+    print("True")
+else:
+    print("False")
+
+def add(a,b):
+    result = a + b
+    return result
+print(add(4,5))
+
+
+# Bài tâp 3
+print("Chao cac ban!\n")
+
+# Bài tập 4
+print("Lê Thanh Hòa\n")
